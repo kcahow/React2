8d4b03a74219b27779bec8de582ada116ee7409b
kcahow <kcahow@gmail.com>
2024-11-15 14:01:23 -0500

    Added more components to snapshot tests

--- src/snapshot.test.tsx
+++ src/snapshot.test.tsx
@@ -1,9 +1,37 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import { Clock } from "./Clock"; // Adjust the import path as necessary
+import { Budget } from "./Budget"; // Adjust the import path as necessary
+import { BudgetSummary } from "./BudgetSummary"; // Adjust the import path as necessary
+import { BudgetItemDataType } from "./api";
 
 test("Clock component snapshot", () => {
   const component = renderer.create(<Clock />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("Budget component snapshot", () => {
+  const component = renderer.create(<Budget monthIndex={11} />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("BudgetSummary component snapshot", () => {
+  type BudgetLineType = {
+    id?: string;
+    desc: string;
+    amount: number;
+  };
+
+  type BudgetItemType = "income" | "expenses" | "debt";
+
+  const mockData: Record<BudgetItemType, BudgetLineType[]> = {
+    income: [],
+    expenses: [],
+    debt: [],
+  };
+  const component = renderer.create(<BudgetSummary data={mockData} />);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
